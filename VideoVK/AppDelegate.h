deab912264b1faaf951f429a0345f90f8eb70a11
kushak <ship93@yandex.ru>
2017-03-17 17:17:38 +0300

    Initial Commit

---
+++ VideoVK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoVK
+//
+//  Created by user on 17.03.17.
+//  Copyright © 2017 Oleg Shipulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoVK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoVK
+//
+//  Created by user on 17.03.17.
+//  Copyright © 2017 Oleg Shipulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
